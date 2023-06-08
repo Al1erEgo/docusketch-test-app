90a416a705127d2a61181912295a35bff037a741
Al1erEgo <al1erego@gmail.com>
2023-06-08 21:18:32 +0200

    feat: add Font Awesome icons and render first icon

--- src/app/random-icon/random-icon.module.ts
+++ src/app/random-icon/random-icon.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RandomIconComponent } from './components/random-icon/random-icon.component';
+import { FontAwesomeModule, FaIconLibrary  } from '@fortawesome/angular-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
 
 
 
@@ -9,8 +12,13 @@ import { RandomIconComponent } from './components/random-icon/random-icon.compon
     RandomIconComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FontAwesomeModule,
   ],
   exports: [RandomIconComponent]
 })
-export class RandomIconModule { }
+export class RandomIconModule {
+  constructor(library: FaIconLibrary) {
+    library.addIconPacks(fas)
+  }
+}
